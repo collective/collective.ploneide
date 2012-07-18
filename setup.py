b0500f588894aedf13136b67c2c538dbd246c410
Franco Pellegrini <frapell@gmail.com>
2012-07-18 12:29:45 -0300

    Rearranged the package structure, added a development buildout and removed the developermanual from the source since we now fetch it using the recipe

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 import os
 
 version = '0.1'
 
+description = "Integrated Development Environment for Plone."
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
+
 setup(name='collective.ploneide',
       version=version,
-      description="Integrated Development Environment for Plone",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description=description,
+      long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -19,7 +26,8 @@ setup(name='collective.ploneide',
       author_email='frapell@gmail.com',
       url='http://svn.plone.org/svn/collective/collective.ploneide/',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
@@ -30,6 +38,9 @@ setup(name='collective.ploneide',
           'TornadIO',
           'ordereddict',
       ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
       entry_points="""
       # -*- Entry points: -*-
 
