89e1494a31b920008135f8674574873c4f4aa0ab
Franco Pellegrini <frapell@gmail.com>
2012-10-08 01:34:37 +0200

    Provide HTML parts inclusion like browser:page and genericsetup:registerProfile

--- src/collective/ploneide/ploneide/zcml_frontend/main.py
+++ src/collective/ploneide/ploneide/zcml_frontend/main.py
@@ -41,61 +41,63 @@ namespace_template = """
     </li>
 """
 
-class ZCMLDirectives(object):
 
-    def __init__(self):
-        cur_dir = os.getcwd()
-        namespaces = os.listdir('directives')
-        self.html = ""
+def ZCMLDirectives():
+    cur_dir = os.getcwd()
+    namespaces = os.listdir('directives')
+    html = ""
 
-        for namespace in namespaces:
-            namespace_dir = os.path.join(cur_dir, 'directives', namespace)
-            directives = ""
+    for namespace in namespaces:
+        namespace_dir = os.path.join(cur_dir, 'directives', namespace)
+        directives = ""
 
-            for directive in os.listdir(namespace_dir):
-                directive_dir = os.path.join(namespace_dir, directive)
-                metadata = open(os.path.join(directive_dir, 'metadata'))
+        for directive in os.listdir(namespace_dir):
+            directive_dir = os.path.join(namespace_dir, directive)
+            metadata = open(os.path.join(directive_dir, 'metadata'))
 
-                # XXX: This needs to be rewritten in a more appropriate way
-                title = ""
-                help_text = ""
-                help_url = ""
+            # XXX: This needs to be rewritten in a more appropriate way
+            title = ""
+            help_text = ""
+            help_url = ""
 
-                for line in metadata:
-                    if line.startswith('title'):
-                        title = line[7:-2]
-                    elif line.startswith('help_text'):
-                        help_text = line[11:-2]
-                    elif line.startswith('help_url'):
-                        help_url = line[10:-2]
+            for line in metadata:
+                if line.startswith('title'):
+                    title = line[7:-2]
+                elif line.startswith('help_text'):
+                    help_text = line[11:-2]
+                elif line.startswith('help_url'):
+                    help_url = line[10:-2]
 
-                directives += directive_template % dict(namespace=namespace,
-                                                        id=directive,
-                                                        title=title,
-                                                        help_url=help_url,
-                                                        help_text=help_text)
+            directives += directive_template % dict(namespace=namespace,
+                                                    id=directive,
+                                                    title=title,
+                                                    help_url=help_url,
+                                                    help_text=help_text)
 
-            self.html += directive_namespace_template % dict(namespace=namespace,
-                                                             directives=directives)
+        html += directive_namespace_template % dict(namespace=namespace,
+                                                    directives=directives)
 
-
-
-    def toString(self):
-        return self.html
+    return html
 
 
-class ZCMLNamespaces(object):
+def ZCMLNamespaces():
+    cur_dir = os.getcwd()
+    namespaces = os.listdir('namespaces')
+    html = ""
 
-    def __init__(self):
-        cur_dir = os.getcwd()
-        namespaces = os.listdir('namespaces')
-        self.html = ""
+    for namespace_id in namespaces:
+        namespace_file = open(os.path.join(cur_dir, 'namespaces', namespace_id))
+        html += namespace_template % dict(namespace=namespace_file.read(),
+                                          id=namespace_id)
 
-        for namespace_id in namespaces:
-            namespace_file = open(os.path.join(cur_dir, 'namespaces', namespace_id))
-            self.html += namespace_template % dict(namespace=namespace_file.read(),
-                                                   id=namespace_id)
+    return html
 
 
-    def toString(self):
-        return self.html
+def ZCMLDirectiveHTML(zcml_directive, index):
+    cur_dir = os.getcwd()
+
+    namespace, directive = zcml_directive.split(":")
+
+    html_part = open(os.path.join(cur_dir, 'directives', namespace, directive, 'zcml_frontend_part.html'))
+
+    return html_part.read() % dict(element_number=index)
